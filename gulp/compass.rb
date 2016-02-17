acbc4c6b8a98e7b84ecf4f2bab9929d89cbacfb0
Joshua Bryant <josh.m.b88@gmail.com>
2016-02-17 16:07:02 -0500

    Gulp.

---
+++ gulp/compass.rb
@@ -0,0 +1,13 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+css_dir = "css"
+fonts_dir = "fonts"
+
+output_style = :compressed
+preferred_syntax = :scss
+line_comments = false
---
+++ gulp/gulpfile.js
@@ -0,0 +1,65 @@
+/**
+ * File: gulpfile.js
+ *
+ * Gulp task runner configuration
+ */
+
+var gulp         = require('gulp'),
+    compass      = require('gulp-compass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    notify       = require('gulp-notify'),
+    uglify       = require('gulp-uglify'),
+    plumber      = require('gulp-plumber'),
+    rename       = require('gulp-rename'),
+    concat       = require('gulp-concat');
+
+
+// TASK: scripts
+gulp.task('scripts', function (){
+    gulp.src('js/src/*.js')
+        .pipe(plumber())
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(rename('main.min.js'))
+        .pipe(gulp.dest('js/dist/'))
+        .pipe(notify({
+          title: "JavaScript processed.",
+          message: "Concatenated and uglified.",
+          sound: "Blow"
+        }));
+});
+
+
+// TASK: styles
+gulp.task('styles', function () {
+    return gulp.src('sass/*.scss')
+        .pipe(compass({
+            config_file: './compass.rb',
+            css: 'css',
+            sass: 'sass',
+            output: 'compressed'
+        }))
+        .on('error', notify.onError({
+          title: "Sass compilation failed.",
+          message: "<%= error.message %>",
+          sound: "Basso"
+        }))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('css'))
+        .pipe(notify({
+          title: "Sass compiled.",
+          message: "File: <%= file.relative %>",
+          sound: "Pop"
+        }));
+});
+
+
+// TASK: watch
+gulp.task('watch', ['styles', 'scripts'], function (){
+    gulp.watch('./sass/**/*.scss', ['styles']);
+    gulp.watch('./js/src/*.js', ['scripts']);
+});
+
+
+// TASK: default
+gulp.task('default', ['watch']);
